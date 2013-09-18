a229e1737542a5011e70c3fa63c360638e96e754
Nick Pilon <npilon@lexmachina.com>
2013-09-18 15:52:51 -0700

    Make the step actually do something.

--- lettuce_webdriver/css_selector_steps.py
+++ lettuce_webdriver/css_selector_steps.py
@@ -1,3 +1,5 @@
+import time
+
 from lettuce import step
 from lettuce import world
 
@@ -7,11 +9,11 @@ from lettuce_webdriver.util import assert_false
 import logging
 log = logging.getLogger(__name__)
 
-def wait_for_elem(browser, xpath, timeout=15):
+def wait_for_elem(browser, sel, timeout=15):
     start = time.time()
     elems = []
     while time.time() - start < timeout:
-        elems = browser.find_elements_by_css_selector(xpath)
+        elems = browser.find_elements_by_css_selector(sel)
         if elems:
             return elems
         time.sleep(0.2)
@@ -20,7 +22,7 @@ def wait_for_elem(browser, xpath, timeout=15):
 @step(r'There should be an element matching \$\("(.*?)"\) within (\d+) seconds?')
 def wait_for_element_by_selector(step, selector, seconds):
     log.error(selector)
-    #elems = wait_for_elem(world.browser, selector, seconds)
-    #assert_true(step, elems)
+    elems = wait_for_elem(world.browser, selector, seconds)
+    assert_true(step, elems)
 
 __all__ = ['wait_for_element_by_selector']
